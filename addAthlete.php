678ec0eb93ea2ee62ef84566e40bb12a35e50bbb
michaelduffy <mictriduf@hotmail.co.uk>
2017-03-30 10:04:31 +0100

    Add files via upload

--- addAthlete.php
+++ addAthlete.php
@@ -12,12 +12,11 @@
 	$email = $_POST['txtEmail'];
 	$password = $_POST['txtPassword'];
 	
-	//print($firstName."--".$lastName."--".$add1."--".$add2."--".$add3."--".$county."--".$gender."--".$dob."--".$phone."--".$email."--".$password);
-	
-	$connection=mysqli_connect("localhost","root",""); //("localhost","d567687_michael","marybary59");
-		
+	//db connection
+	$connection=mysqli_connect("localhost","root",""); 
 	mysqli_select_db($connection,"project_database");
-		
+	
+	//get athletes currently registered	
 	$result = mysqli_query($connection,"select athlete_id, ath_first_name, ath_last_name, phone_num from series_athlete");
 	
 	$newAthlete=true;
@@ -32,35 +31,38 @@
 		$athFirstName=$row['ath_first_name'];
 		$athLastName=$row['ath_last_name'];
 		$athPhone=$row['phone_num'];
-		if( ($athFirstName==$firstName) &&($athLastName==$lastName) && ($athPhone==$phone) )  //check athlete is not already in database
+		if( ($athFirstName==$firstName) &&($athLastName==$lastName) && ($athPhone==$phone) )  //check if athlete is already in database
 		{
-			$newAthlete=false;
+			$newAthlete=false; //if athlete already exists
 		}
 		
 		$currentId=$row['athlete_id'];
-		if($currentId > $highestId)
+		if($currentId > $highestId) //finding the highest athlete ID in the database
 		{
 			$highestId = $currentId;
 		}
 	}
 	
-	$newId = $highestId+1;
+	$newId = $highestId+1; //new athlete ID is the current highest plus 1
 		
-	if($newAthlete)
+	if($newAthlete) //atempting to register an athlete NOT already existing in database
 	{		
 		$addSuccess = mysqli_query($connection,"INSERT INTO series_athlete VALUES($newId, '$firstName', '$lastName', '$add1', '$add2', '$add3', '$county', '$gender', '$dob', '$phone', '$email', '$password')");
-		if($addSuccess == true)
+		if($addSuccess == true) //if add to database was successful
 		{
 			header("Location: getAthleteDetails.php?addSuccess=pass&id=$newId&firstName=$firstName&lastName=$lastName&add1=$add1&add2=$add2&add3=$add3&county=$county&gender=$gender&dob=$dob&phone=$phone&email=$email&password=$password");  
+			//successful registration
 		}
-		else
+		else //if add to database was not successful
 		{
-		     header("Location: getAthleteDetails.php?addSuccess=fail");		 
+		     header("Location: getAthleteDetails.php?addSuccess=fail");
+			//exception/problem while attempting to register		     
 		}
 	}
-	else
+	else //atempting to register an athlete already existing in database
 	{
-	        header("Location: getAthleteDetails.php?addSuccess=invalid");		
+	        header("Location: getAthleteDetails.php?addSuccess=invalid");
+		//atempting to register an athlete already existing in database
 	}  
         mysqli_close($connection);
 ?>
