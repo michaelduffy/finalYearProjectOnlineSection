07130ea53e908f311166e69fcd037871002ba46d
michaelduffy <mictriduf@hotmail.co.uk>
2017-04-09 17:03:31 +0100

    Add files via upload

--- js/sliderJs.js
+++ js/sliderJs.js
@@ -1,47 +1,40 @@
 $( function() 
 {
-	    var $value1=0;
-	    var $value2=0;
-	    var $gender="";
-	    var $minAge=0;
-	    var $maxAge=0;
-	    var $raceId=0;
-	    //var $genderSpan;
+	var $value1=0;
+	var $value2=0;
+	var $gender="";
+	var $minAge=0;
+	var $maxAge=0;
+	var $raceId=0;
 	
 	$("#btnFilter").click(goToFilterPage);//when filter results button is clicked
 	$("#btnFilterStandings").click(goToStandingsWithFilters);//when filter results button is clicked
 	$("#filterArea input[type='radio']").on("change",getGenderValue); //when radio button filter is changed //#filterForm
-	
-	
+		
 	$minAge = $("#minAgeSpan").text(); //getting values to set selected slider values when page is loaded
 	$maxAge = $("#maxAgeSpan").text();
 		
-	function goToStandingsWithFilters() //action on filter results button click
+	function goToStandingsWithFilters() //action on filter standings button click
 	{
 		$minAge = $("#minAgeSpan").text();
-		$minAge=parseInt($minAge);
-		console.log("minAge = "+$minAge);
 		$maxAge = $("#maxAgeSpan").text();
 		$gender = $("#genderSpan").text();
-		//$raceId = $("#raceIdSpan").text();
-		//console.log("id = "+$raceId+", gender = "+$gender+", min = "+$minAge+", max = "+$maxAge+"");
+		//go to page
 		window.location.href = "seriesStandingsPage.php?minAge="+$minAge+"&maxAge="+$maxAge+"&gender="+$gender+"";
 	}
 	
-	function goToFilterPage() //action on filter results button click
+	function goToFilterPage() //action on filter race results button click
 	{
-		$minAge = $("#minAgeSpan").text();
-		$minAge=parseInt($minAge);
-		console.log("minAge = "+$minAge);
+		$minAge = $("#minAgeSpan").text();		
 		$maxAge = $("#maxAgeSpan").text();
 		$gender = $("#genderSpan").text();
 		$raceId = $("#raceIdSpan").text();
-		//console.log("id = "+$raceId+", gender = "+$gender+", min = "+$minAge+", max = "+$maxAge+"");
+		//go to page
 		window.location.href = "filteredResultsPage.php?raceId="+$raceId+"&minAge="+$minAge+"&maxAge="+$maxAge+"&gender="+$gender+"";
 	}
 	
-	
-	    $( "#slider-range" ).slider(
+	//creating and adding dual slider
+	$( "#slider-range" ).slider(
 		{
 		      range: true,
 		      min: 10,
@@ -51,9 +44,6 @@ $( function()
 			{
 				$( "#amount" ).val( "" + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
 				console.log("slidertestIng1");
-				//var value = $( "#minAgeSpan" ).slider( "values", 0 );
-				//$("minAgeSpan").text();
-				//console.log("value 1 = "+value);
 			}
 		});
 		$( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
@@ -63,33 +53,17 @@ $( function()
 		$( "#slider-range" ).slider({
 		  change: function( event, ui ) 
 			{   
-				$value1 = $( "#slider-range" ).slider( "values", 0 );
-				$value2 = $( "#slider-range" ).slider( "values", 1 );
-				//$("minAgeSpan").text();
-				//console.log("value 1 = "+$value1); 
-				//console.log("value 2 = "+$value2); 
-				$("#minAgeSpan").text($value1);
-				$("#maxAgeSpan").text($value2);
-				//console.log("span 1 = "+$("#minAgeSpan").text()); 
-				//console.log("span 2 = "+$("#maxAgeSpan").text()); 
+				$value1 = $( "#slider-range" ).slider( "values", 0 );//get  slider min value
+				$value2 = $( "#slider-range" ).slider( "values", 1 );//get slider max value
+				$("#minAgeSpan").text($value1); //store minAge value in #minAgeSpan of calling page
+				$("#maxAgeSpan").text($value2); //store minAge value in #minAgeSpan of calling page
 			}
 		});
-		
-		//console.log("test = "+$(":checked").val());
-		
-		
-		
-		
-		
-		
+	
 		function getGenderValue() //action on radio button filter change
 		{
-			$gender = $("#filterArea input[type='radio']:checked").val();
-			$("#genderSpan").text($gender);
-			console.log("gender = "+$gender)
-			//console.log("spanValue6 = "+$("#genderSpan").text());
-			
-			//return $gender;
+			$gender = $("#filterArea input[type='radio']:checked").val(); //get selected radio button value
+			$("#genderSpan").text($gender); //store gender value in #genderSpan of calling page
 		}
 		    
     
